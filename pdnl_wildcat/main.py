6f19cbe0646542c54af27e9351c88bae9a5403cc
Noah Capp <noahmcapp@gmail.com>
2026-03-05 19:45:07 -0500

    saving class names within the output array

--- pdnl_wildcat/main.py
+++ pdnl_wildcat/main.py
@@ -19,8 +19,6 @@ def main():
     parser.add_argument('-o', '--output_file', help=".npy path to write outputs to", required=True)
     parser.add_argument('-m', '--model_file', help="path to .dat file which stores model weights", required=True)
     parser.add_argument('-p', '--parameters_file', help="path to .json file which stores model parameters", required=True)
-    parser.add_argument('--save_proba', action='store_true', help="if given, stores the raw probability outputs")
-    parser.add_argument('--debug', action='store_true', help="plots an output image to analyze")
     args = parser.parse_args()
 
     params = json.load(open(args.parameters_file, 'r'))
@@ -29,29 +27,9 @@ def main():
     frame = pdnl_sana.image.Frame(args.input_file)
     out = model.run(frame)
 
-    
-    def softmax(x):
-        e_x = np.exp(x - np.max(x))
-        return e_x / np.sum(e_x, axis=0)
-    proba = softmax(out)
-    
-    if args.save_proba:
-        np.save(os.path.splitext(args.output_file)[0]+'_proba.npy', proba)
+    class_names = params['class_names']
+    arrs = {class_names[i]: out[i] for i in range(len(class_names))}
+    np.savez(os.path.splitext(args.output_file)[0]+'.npz', **arrs)
 
-    pred = np.argmax(proba, axis=0)
-    np.save(os.path.splitext(args.output_file)[0]+'.npy', pred)
-
-    if args.debug:
-        class_names = params['class_names']
-        fig, axs = plt.subplots(1,1+len(class_names)+1, sharex=True, sharey=True)
-        axs[0].imshow(frame.img)
-        axs[0].set_title('Original')
-        for i in range(len(class_names)):
-            axs[1+i].imshow(proba[i], vmin=0, vmax=1)
-            axs[1+i].set_title(class_names[i])
-        axs[-1].imshow(pred, vmin=0, vmax=proba.shape[0])
-        axs[-1].set_title('Predictions')
-        plt.show()
-    
 if __name__ == "__main__":
     main()
